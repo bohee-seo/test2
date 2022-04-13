31f8b50408c17c857fc5985e8ee98bb31376d873
bohee-seo <bo-hee.seo@microfocus.com>
2022-04-13 17:17:08 +0800

    recording

---
+++ data/WebSocketBuffer.h
@@ -0,0 +1,10 @@
+#ifndef VUSER_WEBSOCKET_BUFFER_HEADER
+#define VUSER_WEBSOCKET_BUFFER_HEADER
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+char WebSocketReceive0[] = "{\"messageType\":\"hello\",\"uaid\":\"2682ade580b34ad0a4df63698ef7486f\",\"statu"
+                        "s\":200,\"use_webpush\":true,\"broadcasts\":{}}";
+long WebSocketReceiveLen0   = sizeof(WebSocketReceive0) - 1;	// (record-time: 113 bytes)
+
+#endif
